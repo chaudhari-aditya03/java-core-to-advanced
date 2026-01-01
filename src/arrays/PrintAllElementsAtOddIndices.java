3b0d5cf53308d8f360f70b1aa9f51aadf5a42987
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-01 21:50:22 +0530

    Created PrintELementATOddIndices

--- src/arrays/PrintAllElementsAtOddIndices.java
+++ src/arrays/PrintAllElementsAtOddIndices.java
@@ -1,4 +1,16 @@
 package arrays;
 
 public class PrintAllElementsAtOddIndices {
+    public static void main(String[] args) {
+        int [] arr = new int[101];
+        for(int i =1;i<=100;i++)
+        {
+            arr[i]=i;
+        }
+        for(int i =1;i<=100;i++)
+            if(i%2!=0)
+            {
+                System.out.print(arr[i]+" ");
+            }
+    }
 }
