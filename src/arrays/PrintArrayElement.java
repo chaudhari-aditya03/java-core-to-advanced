7d0c48bb39c6d14f895c1f3deb4fde36e3e356e7
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-01 18:37:19 +0530

    PrintArrayElements Updated

--- src/arrays/PrintArrayElement.java
+++ src/arrays/PrintArrayElement.java
@@ -1,4 +1,20 @@
 package arrays;
 
 public class PrintArrayElement {
+    public static void main(String[] args)
+    {
+        int [] arr = {10,20,30,40,50,60,70,80,90,100};
+        System.out.println("Print Using For Loop");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\nPrint Using While Loop");
+        int a=0;
+        while(a<arr.length)
+        {
+            System.out.print(arr[a]+" ");
+            a++;
+        }
+    }
 }
