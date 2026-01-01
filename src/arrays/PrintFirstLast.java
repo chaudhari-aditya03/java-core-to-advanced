d7531e9a214db776d0c9bb4a929a98f5da2228b7
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-01 19:13:10 +0530

    PrintFirstAndLast updated

--- src/arrays/PrintFirstLast.java
+++ src/arrays/PrintFirstLast.java
@@ -1,4 +1,10 @@
 package arrays;
 
 public class PrintFirstLast {
+public static void main(String[] args)
+{
+    int[] arr = {10,20,30,40,50,60,70,80,90,100};
+    System.out.println(arr[0]);
+    System.out.println(arr[arr.length-1]);
+}
 }
