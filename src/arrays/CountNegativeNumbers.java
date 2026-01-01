9c24588b51d543a74ba55b20ce6c44a29710d526
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-01 21:33:40 +0530

    Created CountNegativeNumbers

---
+++ src/arrays/CountNegativeNumbers.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class CountNegativeNumbers {
+    public static void main(String[] args) {
+        int [] arr ={2,-1,82,-95,75,0,-125,230};
+        System.out.println("Number Of Positive Numbers in Given Array is : ");
+        System.out.println("0 is non positive , non negative number");
+        int count =0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                count++;
+            }
+        }
+        System.out.println("Negative Numbers in Given Arr" +
+                "ay is "+count);
+    }
+}
