99a27fbd841b647f59bec94950dc0c77f320023d
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-01 20:34:06 +0530

    Studied Largest , Second Largest and Third Largest

--- src/arrays/FindSecondLargestElementOfArray.java
+++ src/arrays/FindSecondLargestElementOfArray.java
@@ -2,27 +2,26 @@ package arrays;
 
 public class FindSecondLargestElementOfArray {
     public static void main(String[] args) {
-        int[] arr ={20,60,1200,65,78,901};
-        int Largest=0;
-        int SecondLargest = 0;
-        for(int i =0;i<arr.length-1;i++){
-            for(int j =i+1;j<arr.length;j++){
-                if(arr[i]>arr[j])
-                {
-                    int temp = Largest;
-                    Largest = arr[i];
-                    SecondLargest = temp;
-
-                }
-                else
-                {
-                    int temp = Largest;
-                    Largest = arr[j];
-                    SecondLargest = temp;
-                }
+        int[] arr ={20,60,1200,65,78,901,1050,1051,1600};
+        int Largest= Integer.MIN_VALUE;
+        int SecondLargest = Integer.MIN_VALUE;
+        int ThirdLargest = Integer.MIN_VALUE;
+        for(int i = 1;i<arr.length;i++){
+            if(arr[i]>Largest){
+                ThirdLargest= SecondLargest;
+                SecondLargest = Largest;
+                Largest = arr[i];
+            }
+            else if(arr[i]<Largest && arr[i]>SecondLargest){
+                ThirdLargest = SecondLargest;
+                SecondLargest = arr[i];
+            }
+            else if(arr[i]<SecondLargest && arr[i]>ThirdLargest){
+                ThirdLargest = arr[i];
             }
         }
         System.out.println("Largest Element From Given Array is :"+Largest);
         System.out.println("Second Largest Element is :"+SecondLargest);
+        System.out.println("Third Largest Element is :"+ThirdLargest);
     }
 }
