cbb750e51dda6d43b94bfcd501e437efc0f6a29a
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-01 21:46:01 +0530

    Created PrintELementATEvenIndices

--- src/arrays/PrintElementsAtEvenIndices.java
+++ src/arrays/PrintElementsAtEvenIndices.java
@@ -2,6 +2,15 @@ package arrays;
 
 public class PrintElementsAtEvenIndices {
     public static void main(String[] args) {
-
+        int [] arr = new int[100];
+    for(int i =0;i<=100;i++)
+    {
+        arr[i]=i;
+    }
+    for(int i =0;i<=100;i++)
+        if(i%2==0)
+        {
+            System.out.print(arr[i]+" ");
+        }
     }
 }
