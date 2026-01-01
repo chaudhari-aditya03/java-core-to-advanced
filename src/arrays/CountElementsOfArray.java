9956ed3460db2e964d19418f501abd0eba3ee026
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-01 19:23:46 +0530

    Updated  CountELementsOfArray

--- src/arrays/CountElementsOfArray.java
+++ src/arrays/CountElementsOfArray.java
@@ -1,4 +1,20 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class CountElementsOfArray {
+    static public void main(String []args)
+    {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int num = sc.nextInt();
+        int [] arr = new int[num];
+        System.out.println("Enter the elements in the array:");
+        for(int i=0;i<num;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array Contains "+arr.length+" elements");
+        sc.close();
+    }
 }
