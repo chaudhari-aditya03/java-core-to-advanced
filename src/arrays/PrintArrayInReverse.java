91fd4f41c77bc493b3ff591c036946a06b0f3d05
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-01 18:59:17 +0530

    PrintArrayElementsInReverseOrder Updated

--- src/arrays/PrintArrayInReverse.java
+++ src/arrays/PrintArrayInReverse.java
@@ -1,4 +1,18 @@
 package arrays;
 
 public class PrintArrayInReverse {
+    public static void main(String[] args)
+    {
+    int arr[]={10,20,30,40,50,60,70,80,90,100};
+    System.out.println("Elemets Of Arrays : ");
+    for(int i=0;i<arr.length;i++)
+    {
+        System.out.print(arr[i]+" ");
+    }
+        System.out.println("\nArray Elements in Reverse Order");
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
