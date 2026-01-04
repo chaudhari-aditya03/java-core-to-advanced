47892edd71a9949cebb907d671ba093a32799541
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-04 20:44:05 +0530

    Created Square4 Pattern

--- src/practice/patterns/Square4Pattern.java
+++ src/practice/patterns/Square4Pattern.java
@@ -5,7 +5,7 @@ public class Square4Pattern {
         int num =1;
         for(int i = 1; i <= 5; i++){
             for(int j = 1; j <= 5; j++){
-                System.out.print(num+" ");
+                System.out.print(num+"\t");
                 num++;
             }
             System.out.println();
