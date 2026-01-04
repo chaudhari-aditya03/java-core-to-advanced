488824a20943c18dce76178e5ae93a983bfa1976
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-04 20:42:14 +0530

    Created Square1 Pattern

--- src/practice/patterns/Square1Pattern.java
+++ src/practice/patterns/Square1Pattern.java
@@ -1,4 +1,14 @@
 package practice.patterns;
 
 public class Square1Pattern {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++){
+            int num =1;
+            for(int j = 1; j <= 5; j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
 }
