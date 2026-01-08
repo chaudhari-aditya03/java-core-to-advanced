8fdee6cefdb9305d0c83286c0716586317628876
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-08 23:47:51 +0530

    Created

---
+++ src/arrays/JaggedArray1.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class JaggedArray1 {
+    public static void main(String[] args)
+    {
+        int [][] arr = new int[3][];
+        arr[0]=new int[4];
+        arr[1]=new int[1];
+        arr[2]=new int[3];
+        int num =1;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                arr[i][j]= num;
+                num++;
+            }
+        }
+        for(int i = 0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
