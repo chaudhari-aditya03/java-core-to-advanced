a01365b33945ef391c15ba1725ffba3ed42f4e54
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-08 11:36:24 +0530

    First Commit

--- src/hashmap/CountFrequencyOfEachElementOfArray.java
+++ src/hashmap/CountFrequencyOfEachElementOfArray.java
@@ -1,5 +1,16 @@
 package hashmap;
 
+import java.util.HashMap;
+
 public class CountFrequencyOfEachElementOfArray {
-    
+    public static void main(String[] args)
+    {
+        int count =1;
+        HashMap<Integer,Integer> h1 = new HashMap<>();
+        int [] arr ={10,20,30,40,40,60,60,30,20};
+        for(int i=0;i<arr.length;i++) {
+            h1.put(arr[i], count);
+        }
+        System.out.println(h1);
+    }
 }
