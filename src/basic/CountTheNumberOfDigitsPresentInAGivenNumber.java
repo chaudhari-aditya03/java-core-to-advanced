6e177f068d3e8d69f8d6af325538da8c109c4aa4
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-15 21:42:47 +0530

    First Commit

--- src/basic/CountTheNumberOfDigitsPresentInAGivenNumber.java
+++ src/basic/CountTheNumberOfDigitsPresentInAGivenNumber.java
@@ -1,4 +1,23 @@
 package basic;
 
+import java.util.Scanner;
+
 public class CountTheNumberOfDigitsPresentInAGivenNumber {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int n = sc.nextInt();
+        int a= n;
+        int count =0;
+        int num = 0;
+        while(n>0)
+        {
+            num = n%10;
+            count++;
+            n = n/10;
+        }
+        System.out.println(a+" contains "+count+" Digits");
+        sc.close();
+    }
 }
