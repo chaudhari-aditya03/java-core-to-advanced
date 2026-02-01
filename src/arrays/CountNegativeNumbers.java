8639670ba25d1b85f3f060eaea447145b8edb455
Aditya Chaudhari <adityachaudhariedu312005@gmail.com>
2026-02-01 23:29:33 +0530

    Fix formatting in main method of CountNegativeNumbers

--- src/arrays/CountNegativeNumbers.java
+++ src/arrays/CountNegativeNumbers.java
@@ -2,7 +2,8 @@ package arrays;
 
 public class CountNegativeNumbers
     {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+        {
         int [] arr ={2,-1,82,-95,75,0,-125,230};
         System.out.println("Number Of Positive Numbers in Given Array is : ");
         System.out.println("0 is non positive , non negative number");
