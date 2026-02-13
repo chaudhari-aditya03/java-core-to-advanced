cade88d9c0c2ec026d4ef8c5c2cdaf1454943792
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-02-13 16:24:43 +0530

    List Done

---
+++ src/collections/list/A2.java
@@ -0,0 +1,14 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A2 {
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+    }
+}
