126cf06a2a8927cf5b32fe08e8ea2bc9d2c2212a
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-02-14 22:59:01 +0530

    Done

--- src/arrays/SubArray.java
+++ src/arrays/SubArray.java
@@ -1,4 +1,15 @@
 package arrays;
 
 public class SubArray {
+    public static void main(String[] args){
+        int[] arr ={10,20,30,40,50,60};
+        for(int start=0;start<arr.length;start++){
+            for(int end = start;end<arr.length;end++ ){
+                for(int i = start ;i<=end;i++ ){
+                    System.out.print(arr[i]);
+                }
+                System.out.println();
+            }
+        }
+    }
 }
