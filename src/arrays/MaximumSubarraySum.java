e73cc3ad268a6603949646967108da4ede5a48ab
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-02-14 22:56:40 +0530

    Done:

--- src/arrays/MaximumSubarraySum.java
+++ src/arrays/MaximumSubarraySum.java
@@ -1,4 +1,16 @@
 package arrays;
 
 public class MaximumSubarraySum {
+    public static void main(String[] arsg){
+        int[] arr = {1,2,3,5};
+        int MaxSum = Integer.MIN_VALUE;
+        for(int start=0;start<arr.length;start++ ){
+            int currSum =0;
+            for(int end =start+1;end<arr.length;end++ ){
+                currSum+=arr[end];
+                MaxSum = Math.max(currSum,MaxSum);
+            }
+        }
+        System.out.println(MaxSum);
+    }
 }
