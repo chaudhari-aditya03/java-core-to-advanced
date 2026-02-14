acf745f813860be639b5ac1c7de2bcc89f029dff
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-02-14 22:56:28 +0530

    Done:

--- src/arrays/IntersectionOfTwoArrays.java
+++ src/arrays/IntersectionOfTwoArrays.java
@@ -1,4 +1,18 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class IntersectionOfTwoArrays {
+    public static void main(String[] args){
+        int[] arr1={1,2,3,4};
+        int[] arr2={5,2,3,7};
+        for(int i =0;i<arr1.length;i++){
+            for(int j =0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j]){
+                    System.out.print(arr1[i]+"\t");
+                }
+            }
+
+        }
+    }
 }
